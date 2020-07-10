4778ffb9664ab03121ba3d64da21aa15ff235763
primemini <vjvitale@buffalo.edu>
2020-07-10 11:43:56 -0400

    created templates for menus and connected all pages to server

--- backend/server.py
+++ backend/server.py
@@ -11,28 +11,64 @@ import random
 import hashlib
 
 app = Bottle()
+#base of where all static pages are served from
+file_root = "../frontend"
+#base of where all templates are served from
+bottle.TEMPLATE_PATH.insert(0, "../frontend")
+#resets templates between server restarts
+bottle.TEMPLATES.clear()
 #serves the home page where users may look up a restuarnt
 @app.get('/')
 def serve_home():
-	return static_file("index.html", root="ADD ROOT FOR HTML", mimetype="text/html")
+	return static_file("search.html", root=file_root, mimetype="text/html")
 #servers the login for the restuarnt
 @app.get('/login')
 def serve_login():
-	return static_file("login.html", root="ADD ROOT FOR HTML", mimetype="text/html")
+	return static_file("login.html", root=file_root, mimetype="text/html")
 
 #serves the restuarunt's menu
 
-#THIS WILL BE A TEMPLATE
-@app.get('/r/<resturant_name>')
+@app.get('/m/<resturant_name>')
+@view("view-menu.tpl")
 def serve_resturant(resturant_name):
-	return "RETURN THE TEMPLATE"
+	"""
+	Template format
+
+	a dictionary with the keys name and items
+	name  holds a string, which is the name of the restuarnt
+	items holds dictionary of lists
+		each key is a section header
+		each list contains tuples that has the item and price of each item and a description
+	"""
+	retVal = {"name" : "", "items":{}}
+
+	retVal["name"] = "Eggs Dee"
+	retVal["items"] = {"Drinks": [("soda", "2.50", "Choices of Pepsi Products"), ("tea", "1.50", "Choices of Chai, Green, or Black")], "Main Courses": [("Omlet", "7.99", "Eggs choice of tomatoes, mushrooms"), ("Eggs Benedict", "15.00", "Poached eggs on an english muffin with canadian bacon and hollandaise sauce")]}
+	return retVal
 
 #allows the restuarant to edit their menu
 
-#THIS WILL ALSO BE A TEMPLATE
 #NEED TO VERIFY THE USER HAS LOGGED IN
 @app.get('/e/<resturant_name>')
-def serve_resturant(resturant_name):
-	return "RETURN THE TEMPLATE"
+@view("menu.tpl")
+def serve_resturant_edits(resturant_name):
+	"""
+	Template format
+
+	a dictionary with the keys name and items
+	name  holds a string, which is the name of the restuarnt
+	items holds dictionary of lists
+		each key is a section header
+		each list contains tuples that has the item and price of each item and a description
+	"""
+	retVal = {"name" : "", "items":{}}
+
+	retVal["name"] = "Eggs Dee"
+	retVal["items"] = {"Drinks": [("soda", "2.50", "Choices of Pepsi Products"), ("tea", "1.50", "Choices of Chai, Green, or Black")], "Main Courses": [("Omlet", "7.99", "Eggs choice of tomatoes, mushrooms"), ("Eggs Benedict", "15.00", "Poached eggs on an english muffin with canadian bacon and hollandaise sauce")]}
+	return retVal
+
+@app.get('/style.css')
+def serve_style():
+	return static_file("style.css", root=file_root, mimetype="text/css")
 
 app.run(host='localhost', port=8080)
---
+++ frontend/menu.tpl
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+<link rel="stylesheet" href=style.css>
+<html>
+    <head>
+        <title>Edit Menu</title>
+    </head>
+    <body>
+        <div>
+            <h2>Resturant Name</h2>
+            <h2>{{name}}</h2>
+            %for item in items.keys():
+            <div class = "row, padding-left">
+                <h4>{{item}}</h4>
+            </div>
+                <ul style="list-style: none;" class = "padding-bottom">
+                    %for vals in items[item]:
+                    <li class = "padding-bottom">{{vals[0]}} ... {{vals[1]}}
+                        <br><span>{{vals[2]}}</span> <button type="button" class="btn btn-danger">delete</button></li>
+                    %end
+                </ul>
+            %end
+            <div>
+                <button type="button" class="btn btn-success">Save</button>
+            </div>
+        </div>
+    </body>
+</html>
